06e3b72b447d8d3a471bfcae9112fbae9c478f9d
dpzmick <dpzmick@gmail.com>
2015-04-21 01:15:22 -0500

    generate

--- confs/gen.py
+++ confs/gen.py
@@ -1,22 +1,22 @@
 def weight(x,y):
     if x == y:
-        return 10
+        return 10.0
     else:
         return 'DEFAULT'
 
 if __name__ == "__main__":
-    xmax = 1000
-    ymax = 1000
+    xmax = 500
+    ymax = 500
     numants = 500
     runtime = 60
-    default = 1
+    default = 1.0
 
     f = open('confs/generated.conf', "w")
     f.write("Xmax: %d\n" % xmax)
     f.write("Ymax: %d\n" % ymax)
     f.write("NumAnts: %d\n" % numants)
     f.write("Runtime: %d\n" % runtime)
-    f.write("DefaultWeight: %d\n" % default)
+    f.write("DefaultWeight: %f\n" % default)
     f.write("\n")
     f.write("cell_x,cell_y,cell_weight")
 
@@ -25,6 +25,6 @@ if __name__ == "__main__":
             v = weight(x,y)
 
             if v != 'DEFAULT':
-                f.write("%d,%d,%d\n" % (x,y,v))
+                f.write("%d,%d,%f\n" % (x,y,v))
 
     f.close()
