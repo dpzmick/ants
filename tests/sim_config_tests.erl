f6f24358f513441c5010bf0145e6165c71934c9a
dpzmick <dpzmick@gmail.com>
2015-04-20 19:20:43 -0500

    test for config file

---
+++ tests/sim_config_tests.erl
@@ -0,0 +1,27 @@
+-module(sim_config_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+%% Assumes run from the root (test script)
+
+read_a_test_() ->
+    {
+     "read test_data/a.conf",
+     fun () ->
+             Conf = sim_config:from_file("tests/test_data/a.conf"),
+             Xmax = sim_config:xmax(Conf),
+             Ymax = sim_config:ymax(Conf),
+             ?assert(Xmax == 10),
+             ?assert(Ymax == 10),
+             ?assert(sim_config:numants(Conf) == 10),
+             ?assert(sim_config:runtime(Conf) == 30),
+             Coords = [{X,Y} || X <- lists:seq(1,Xmax), Y <- lists:seq(1, Ymax)],
+             lists:map(
+               fun ({X,Y}) ->
+                       if
+                           X == Y -> ?assert(sim_config:cell_weight(Conf,X,Y) == 10);
+                           true -> ?assert(sim_config:cell_weight(Conf, X, Y) == 1)
+                       end
+               end,
+               Coords)
+     end
+    }.
