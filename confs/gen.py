78f1a6c35f115ea98973e3213b6b5509a41a20e3
dpzmick <dpzmick@gmail.com>
2015-04-21 13:46:30 -0500

    ugh

--- confs/gen.py
+++ confs/gen.py
@@ -1,13 +1,13 @@
 def weight(x,y):
     if x == y:
-        return 10.0
+        return 100.0
     else:
         return 'DEFAULT'
 
 if __name__ == "__main__":
-    xmax = 500
-    ymax = 500
-    numants = 500
+    xmax = 250
+    ymax = 250
+    numants = 250
     runtime = 60
     default = 1.0
 
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     f.write("Runtime: %d\n" % runtime)
     f.write("DefaultWeight: %f\n" % default)
     f.write("\n")
-    f.write("cell_x,cell_y,cell_weight")
+    f.write("cell_x,cell_y,cell_weight\n")
 
     for x in xrange(1,xmax+1):
         for y in xrange(1, ymax+1):
