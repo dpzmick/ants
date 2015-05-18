dc80d32698aedb85f0928b24c13218da7c8fa4a8
dpzmick <dpzmick@gmail.com>
2015-05-17 23:27:53 -0500

    update config stuff

--- src/sim_config.erl
+++ src/sim_config.erl
@@ -1,5 +1,5 @@
 -module(sim_config).
--export([from_file/1, xmax/1, ymax/1, numants/1, runtime/1, cell_weight/3]).
+-export([from_file/1, xmax/1, ymax/1, numants/1, runtime/1, cell_has_food/3, cell_weight/3]).
 
 %% you'd better hope the file is formatted correctly
 
@@ -32,17 +32,20 @@ readline_helper(Fd, Expected, float) ->
     end.
 
 handle_cell_line(Line) ->
-    [X,Y,Weight] = string:tokens(string:strip(Line, both, $\n), ","),
-    [{Ox, _}, {Oy, _}, {Ow, _}] = [string:to_integer(X), string:to_integer(Y), string:to_float(Weight)],
-    [Ox, Oy, Ow].
+    [X,Y,WeightOrFood] = string:tokens(string:strip(Line, both, $\n), ","),
+    [{Ox, _}, {Oy, _}] = [string:to_integer(X), string:to_integer(Y)],
+    case string:to_integer(WeightOrFood) of
+        {error, no_integer} -> [Ox, Oy, food];
+        {Ow, _} -> [Ox, Oy, Ow]
+    end.
 
 iter_cell_config(Fd) -> iter_cell_config(Fd, dict:new()).
 
 iter_cell_config(Fd, Dict) ->
     case file:read_line(Fd) of
         {ok, Line} ->
-            [X,Y,Weight] = handle_cell_line(Line),
-            iter_cell_config(Fd, dict:store({X,Y}, Weight, Dict));
+            [X,Y,V] = handle_cell_line(Line),
+            iter_cell_config(Fd, dict:store({X,Y}, V, Dict));
         eof -> Dict;
         {error, Reason} -> {error, Reason}
     end.
@@ -72,8 +75,14 @@ ymax({_,Ymax,_,_,_,_}) -> Ymax.
 numants({_,_,NumAnts,_,_,_}) -> NumAnts.
 runtime({_,_,_,Runtime,_,_}) -> Runtime.
 
+cell_has_food({_,_,_,_,_,Bound}, X, Y) ->
+    case dict:find({X,Y}, Bound) of
+        {ok, food} -> true;
+        _ -> false
+    end.
+
 cell_weight({_,_,_,_,DefaultWeight,Bound}, X, Y) ->
     case dict:find({X,Y}, Bound) of
-        {ok, Weight} -> Weight;
+        {ok, Weight} when is_number(Weight) -> Weight;
         error -> DefaultWeight
     end.
